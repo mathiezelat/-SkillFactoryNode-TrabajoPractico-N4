ef118e107cc16d55da5e2d38eec6c71d68a8a959
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2022-09-16 01:25:15 -0300

    :hammer: fix: config.js

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const {
-	PORT = 3000,
-	SESSION_SECRET = 'secreto',
-	REDIS_CLIENT_HOST = 'localhost',
-	REDIS_CLIENT_POST = 6379,
+	PORT,
+	SESSION_SECRET,
+	REDIS_CLIENT_HOST,
+	REDIS_CLIENT_POST,
 	GOOGLE_CLIENT_ID,
 	GOOGLE_CLIENT_SECRET,
 	GOOGLE_CLIENT_REDIRECT,
